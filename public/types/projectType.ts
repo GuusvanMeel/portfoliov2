fc600591e66956dad876b603859235b61dc9f1a8
GuusvanMeel <vanmeelguus@gmail.com>
2026-06-16 15:17:02 +0200

    init

---
+++ public/types/projectType.ts
@@ -0,0 +1,11 @@
+export type projectType = {
+  id: number;
+  title: string;
+  imagesrc: string;
+  description: string;
+  tags: string[];
+  duration: string;
+  Github_Link: string;
+  is_hidden: boolean;
+  updated_at: string;
+};
