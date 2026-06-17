ff4d04ce74fdb46ee86403589739fc24eb250b4b
GuusvanMeel <vanmeelguus@gmail.com>
2026-06-17 18:52:45 +0200

    fixed lighthouse

--- lighthouse-auth.js
+++ lighthouse-auth.js
@@ -1,5 +1,7 @@
 // lighthouse-auth.js
+const { loadEnvConfig } = require ("@next/env");
 
+loadEnvConfig(process.cwd());
 module.exports = async (browser) => {
   const page = await browser.newPage();
 
@@ -7,11 +9,11 @@ module.exports = async (browser) => {
 const password = process.env.ADMIN_PASSWORD;
 
 if (!email || !password) {
-  throw new Error("Missing LHCI_ADMIN_EMAIL or LHCI_ADMIN_PASSWORD");
+  throw new Error("Missing =ADMIN_EMAIL or =ADMIN_PASSWORD");
+  
+  };
   await page.goto("http://localhost:3000/login", {
-    waitUntil: "networkidle0"
-  });
-
+    waitUntil: "networkidle0"})
   await page.type('input[name="email"]', process.env.ADMIN_EMAIL);
   await page.type('input[name="password"]', process.env.ADMIN_PASSWORD);
 
@@ -24,4 +26,4 @@ await page.waitForFunction(
 );
 
   await page.close();
-};}
+};
